3f4c76f7fa3aa3a740bd875d9258ba1f2608207e
joel <joelthousend@gmail.com>
2024-02-02 16:32:50 +0300

    learning how flutter ui widget and layout works

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,78 @@ void main() {
         ),
       ),
 
-      body: Center(
-          child: Text(
-              "Hello There",
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.red[400],
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              fontFamily: "MyFont"
-            ),
-          ),
+      body:
+      //  Container(
+      //   color: Colors.purple,
+      //  // padding: const EdgeInsets.sym(20.0),
+      // //  padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 50.0),
+      // padding: const EdgeInsets.fromLTRB(20.0, 50.0, 100.0, 150.0),
+      // margin: const EdgeInsets.all(20.0),
+      //   child: const Text("suck off",
+      //   style: TextStyle(
+      //     color: Colors.white,
+      //   ),
+      //   ),
+      // ), 
+      //const Center(z
+        // child: Image(
+        //   image: AssetImage("assets/ds.jpg"),
+        //   fit: BoxFit.fitHeight,
+        // ), 
+        // child: Image(
+        //   fit:  BoxFit.fill,
+        //   image: NetworkImage("https://images.pexels.com/photos/12730085/pexels-photo-12730085.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"),
+        // ),
+        // child:IconButton(
+        //   onPressed: (){
+        //     print("hi there");
+        //   },
+        //   icon: const Icon(Icons.warning_amber),
+        //   color: Colors.amber,
+        //   ),
+        // child: TextButton.icon(
+        //   label: const Text("hi there"),
+        //   icon: const Icon(
+        //     Icons.ac_unit_sharp,
+        //     size: 50.0,
+        //     color: Colors.red,
+        //   ),
+        //   onPressed: () {
+        //     print('button Pressed');
+        //   }, 
+        //   style: const ButtonStyle(
+        //     backgroundColor: MaterialStatePropertyAll(Colors.blue),
+        //   ),
+        //   // child: const Text("Hi There",
+        //   // style:  TextStyle(
+        //   //   color: Colors.green,
+        //   // ),
+        //   // ),
+        //   ),
+        // child: ElevatedButton(
+        //   onPressed: null,
+        //   style: ButtonStyle(
+        //     backgroundColor: MaterialStatePropertyAll(Colors.red),
+        //   ),
+        //   child:Text(
+        //     "hi there",
+        //     style:TextStyle(
+        //       color: Colors.white,
+        //     ) ,
+        //     ),
+        // ),
+          // child: Text(
+          //     "Hello There",
+          //   style: TextStyle(
+          //     fontSize: 20.0,
+          //     color: Colors.red[400],
+          //     fontWeight: FontWeight.bold,
+          //     letterSpacing: 2.0,
+          //     fontFamily: "MyFont"
+          //   ),
+          // ),
 
-      ),
+     // ),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         backgroundColor: Colors.green,
@@ -33,7 +92,9 @@ void main() {
           color: Colors.white,
         ),
         ),
+      
       ),
+   
     ),
   ));
 }
