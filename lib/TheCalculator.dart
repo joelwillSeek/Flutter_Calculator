d58df2f8a078b0b592ba98ca721d5d1189f3bf89
joel <joelthousend@gmail.com>
2024-02-12 09:49:46 +0300

    finished the arthimatic operations but didnt finish bracket logic

--- lib/TheCalculator.dart
+++ lib/TheCalculator.dart
@@ -10,8 +10,11 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  String display = "";
+  final String initialValue = "0.0";
+  String display = "0.0";
+  String previouslyEvaluated = "0.0";
   bool inBracket = false;
+  String operationTodo = "";
 
   final List<String> listOfNumberAndOperand = const <String>[
     "C",
@@ -38,12 +41,11 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Container(
-          margin: const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
-          padding: const EdgeInsets.all(30.0),
-          height: 200,
+          padding: const EdgeInsets.all(10.0),
+          height: 100,
           alignment: Alignment.centerRight,
           child: Text(
             display,
@@ -54,38 +56,52 @@ class _CalculatorState extends State<Calculator> {
             ),
           ),
         ),
-        const Row(children: [
-          IconButton(
-            onPressed: null,
-            icon: Icon(
-              Icons.access_time_outlined,
-              color: Color(0xFF836d56),
-            ),
-          ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(
-              Icons.rule,
-              color: Color(0xFF836d56),
-            ),
-          ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(
-              Icons.science_outlined,
-              color: Color(0xFF836d56),
-            ),
-          ),
-          IconButton(
-            padding: EdgeInsets.only(left: 200.0),
-            onPressed: null,
-            icon: Icon(
-              Icons.backspace,
-              color: Color(0xFF836d56),
-            ),
-          ),
-        ]),
+        Container(
+          margin: const EdgeInsets.all(10.0),
+          height: 60,
+          alignment: Alignment.centerRight,
+          child: Text(previouslyEvaluated,
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                fontSize: 50.0,
+                color: Colors.grey,
+              )),
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: Row(children: [
+              const IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.access_time_outlined,
+                  color: Color(0xFF836d56),
+                ),
+              ),
+              const IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.rule,
+                  color: Color(0xFF836d56),
+                ),
+              ),
+              const IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.science_outlined,
+                  color: Color(0xFF836d56),
+                ),
+              ),
+              IconButton(
+                padding: const EdgeInsets.only(left: 200.0),
+                onPressed: backSpaceDisplay,
+                icon: const Icon(
+                  Icons.backspace,
+                  color: Color(0xFF836d56),
+                ),
+              ),
+            ])),
         GridView.builder(
+            padding: const EdgeInsets.only(top: 25.0),
             itemCount: 20,
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
@@ -95,32 +111,82 @@ class _CalculatorState extends State<Calculator> {
                   textInButton: listOfNumberAndOperand.elementAt(index),
                   display: display,
                   numberAndOperandClicked: () {
-                    numberAndOperandClicked(index);
+                    numberAndOperandClicked(
+                        listOfNumberAndOperand.elementAt(index));
                   },
                 )),
       ],
     );
   }
 
-  void numberAndOperandClicked(int index) {
+  void backSpaceDisplay() {
+    String removeLastCharacter = display.substring(0, display.length - 1);
     setState(() {
-      String symbol = listOfNumberAndOperand.elementAt(index);
-
-      switch (symbol) {
-        case "()":
-          if (inBracket) {
-            display += ")";
-          } else {
-            display += "(";
-          }
-          inBracket = !inBracket;
-          break;
-        case "C":
-          display = "";
-          break;
-        default:
-          display += symbol;
+      if (removeLastCharacter.isNotEmpty) {
+        display = removeLastCharacter;
+      } else {
+        display = initialValue;
       }
     });
+  }
+
+  double? evaluate(double firstValue, double secondValue, String symbol) {
+    switch (symbol) {
+      case "+":
+        return firstValue + secondValue;
+      case "-":
+        return firstValue - secondValue;
+      case "x":
+        return firstValue * secondValue;
+      case "/":
+        return firstValue / secondValue;
+      default:
+        return null;
+    }
+  }
+
+  void numberAndOperandClicked(String symbol) {
+    switch (symbol) {
+      case "+" || "-" || "x" || "/" || "%":
+        if (previouslyEvaluated != initialValue &&
+            previouslyEvaluated.isNotEmpty) {
+          double? result = evaluate(
+              double.parse(previouslyEvaluated), double.parse(display), symbol);
+          if (result == null) {
+            print("Wrong Operation.");
+          } else {
+            setState(() {
+              previouslyEvaluated = result.toString();
+              display = initialValue;
+            });
+          }
+        } else {
+          setState(() {
+            previouslyEvaluated == initialValue
+                ? previouslyEvaluated = ""
+                : null;
+            previouslyEvaluated = display;
+            display = initialValue;
+            operationTodo = symbol;
+          });
+        }
+        break;
+
+      case "C":
+        setState(() {
+          display = initialValue;
+          previouslyEvaluated = initialValue;
+        });
+
+        break;
+
+      case "=":
+        break;
+      default:
+        setState(() {
+          display == initialValue ? display = "" : null;
+          display += symbol;
+        });
+    }
   }
 }
